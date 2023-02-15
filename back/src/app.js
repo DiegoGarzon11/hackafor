dce8821cb057892c8ff2767ea7e5dbeb930d9fcd
Diego Garzon <diegogarzon357@gmail.com>
2023-02-15 12:52:27 -0500

    Datos Api

--- back/src/app.js
+++ back/src/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const rutes = require('./routes/todos.js');
 const app = express();
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+});
 app.use('/', rutes);
 
 app.listen('3000', () => {
