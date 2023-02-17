1847eada9f8d7254dc02fab8597955d4e73be8b9
sebastian <sebasga12345@gmail.com>
2023-02-17 14:01:39 -0500

    arreglué un poco del main

--- front/src/components/buscador.jsx
+++ front/src/components/buscador.jsx
@@ -17,19 +17,17 @@ export function Buscador(props) {
 	};
 
 	return (
-		<form
-			className='flex flex-wrap gap-8 p-5 items-center justify-center '
-			onSubmit={handleBuscarClick}>
-			<div className='flex items-center border-b border-teal-500 py-2'>
+		<form className='flex p-5 justify-center ' onSubmit={handleBuscarClick}>
+			<div className=' border-b border-teal-500 py-2'>
 				<input
-					className='appearance-none bg-transparent border-none w-full text-lg text-white mr-3 py-1 px-2 leading-tight focus:outline-none'
+					className='bg-transparent text-lg text-white mr-3 py-1 px-2  focus:outline-none'
 					placeholder='Search...'
 					type='text'
 					value={inputValue}
 					onChange={(event) => setInputValue(event.target.value)}
 				/>
 				<button
-					className='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded'
+					className='bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded'
 					type='submit'
 					onClick={handleBuscarClick}>
 					Search
